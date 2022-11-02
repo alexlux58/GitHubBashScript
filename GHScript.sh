61c19d8ed826ff489b706d1323e4a31519e3ac30
alexlux58 <alexlux@live.com>
2022-11-01 20:42:13 -0700

    added gitUpdateRepot fucntion

--- GHScript.sh
+++ GHScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gitCreateNewRepoFunc() {
+gitCreateNewRepo() {
     FILE=README.md
     if [ -f "$FILE" ]; then
         return
@@ -21,7 +21,7 @@ gitCreateNewRepoFunc() {
     fi
 }
 
-gitPushExistingRepoFunc() {
+gitPushExistingRepo() {
     echo "Please enter your GitHub repositories link"
     read link
     git remote add origin "$link"
@@ -29,15 +29,27 @@ gitPushExistingRepoFunc() {
     git push -u origin main
 }
 
-echo "Do you want to (Enter a 1 or 2):\n"
+gitUpdateRepo() {
+    git status
+    git add .
+    echo "Please enter your commit message"
+    read message
+    git commit -m "$message"
+    git push -u origin main
+}
+
+echo "Do you want to (Enter a 1, 2, 3):\n"
 echo "1. Create a new repository"
 echo "2. Push an existing repository"
+echo "3. Update an existing repository"
 read choice
 
 if [[ $choice == 1 ]]; then
-    gitCreateNewRepoFunc
+    gitCreateNewRepo
 elif [[ $choice == 2 ]]; then
-    gitPushExistingRepoFunc
+    gitPushExistingRepo
+elif [[ $choice == 3 ]]; then
+    gitUpdateRepo
 else
     echo "Invalid choice"
 fi
