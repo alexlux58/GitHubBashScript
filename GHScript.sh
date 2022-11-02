56f789ceecf6f551916cff19a38a07dcf8398935
alexlux58 <alexlux@live.com>
2022-11-01 20:04:59 -0700

    first commit

--- GHScript.sh
+++ GHScript.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-FILE=README.md
-
-gitfunc() {
+gitCreateNewRepoFunc() {
+    FILE=README.md
     if [ -f "$FILE" ]; then
         return
     else
@@ -15,8 +14,32 @@ gitfunc() {
         git add .
         git commit -m "$message"
         git branch -M main
+        echo "Please enter your GitHub repositories link"
+        read link
+        git remote add origin "$link"
+        git push -u origin main
     fi
 }
 
-gitfunc
-echo "success"
+gitPushExistingRepoFunc() {
+    echo "Please enter your GitHub repositories link"
+    read link
+    git remote add origin "$link"
+    git branch -M main
+    git push -u origin main
+}
+
+echo "Do you want to (Enter a 1 or 2):\n"
+echo "1. Create a new repository"
+echo "2. Push an existing repository"
+read choice
+
+if [[ $choice == 1 ]]; then
+    gitCreateNewRepoFunc
+elif [[ $choice == 2 ]]; then
+    gitPushExistingRepoFunc
+else
+    echo "Invalid choice"
+fi
+
+echo "finished"
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# readme
+# This is a bash script I wrote to practice and to create a tool I can use for GitHub.
